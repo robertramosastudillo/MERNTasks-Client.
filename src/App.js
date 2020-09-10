cb9656406265aee7f58497ad0bbcaff7fea5f56c
Robert Andres Ramos Astudillo <rramos.playtechla@gmail.com>
2020-09-10 15:47:30 -0500

    Se crearon validaciones

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ import NuevaCuenta from "./components/auth/NuevaCuenta";
 import Proyectos from "./components/proyectos/Proyectos";
 import ProyectoState from "./context/proyectos/ProyectoState";
 import TareaState from "./context/tareas/TareaState";
+import AlertaState from "./context/alertas/alertaState";
+import AuthState from "./context/autenticacion/authState";
 
 function App() {
+
   return (
     <ProyectoState>
       <TareaState>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-            <Route exact path="/proyectos" component={Proyectos} />
-          </Switch>
-        </Router>
+        <AlertaState>
+          <AuthState>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+                <Route exact path="/proyectos" component={Proyectos} />
+              </Switch>
+            </Router>
+          </AuthState>
+        </AlertaState>
       </TareaState>
     </ProyectoState>
   );
--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,7 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
+import AlertaContext from "../../context/alertas/alertaContext";
+import AuthContext from '../../context/autenticacion/authContext';
 
 const NuevaCuenta = () => {
+  // Extraer los valores del context
+  const alertaContext = useContext(AlertaContext);
+  const { alerta, mostrarAlerta } = alertaContext;
+
+  const authContext = useContext(AuthContext);
+  const { registrarUsuario } = authContext;
+
   // State para iniciar sesion
   const [usuario, guardarUsuario] = useState({
     nombre: "",
@@ -22,19 +31,45 @@ const NuevaCuenta = () => {
 
   // Cuando el usuario quiere iniciar sesion
   const onSubmit = (e) => {
-    e.prevenDefault();
+    e.preventDefault();
 
     // Validar que no haya campos vacios
+    if (
+      nombre.trim() === "" ||
+      email.trim() === "" ||
+      password.trim() === "" ||
+      confirmar.trim() === ""
+    ) {
+      mostrarAlerta("Todos los campos son obligatorios", "alerta-error");
+      return;
+    }
 
     // Password minimo de 6 caracteres
+    if (password.length < 6) {
+      mostrarAlerta(
+        "El password debe ser de al menos 6 caracteres",
+        "alerta-error"
+      );
+      return;
+    }
 
     // Los 2 passwords son iguales
+    if (password !== confirmar) {
+      mostrarAlerta("Los passwords no son iguales", "alerta-error");
+      return;
+    }
 
     //  Pasarlo al action
+    registrarUsuario({
+      nombre, email, password
+    });
   };
 
   return (
     <div className="form-usuario">
+      {alerta ? (
+        <div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div>
+      ) : null}
       <div className="contenedor-form sombra-dark">
         <h1>Obtener una cuenta</h1>
 
